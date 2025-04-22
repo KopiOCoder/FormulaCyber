41be611fb2c7311e78c94a973afc894a4b00bc13
Yuuki-lost <preston1329@gmail.com>
2025-04-22 21:15:00 +0800

    UI in work

---
+++ main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://testing_level.tscn")
+
+
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://settings_menu.tscn")
+
+
+
+
+func _on_tutorial_pressed() -> void:
+	pass # Replace with function body.
