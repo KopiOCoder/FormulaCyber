d28c82bc032939af60c9e49c8a9cfe046415deea
KopiOCoder <wesleylimkopi@gmail.com>
2025-04-22 21:33:03 +0800

    Changed car model, shifted center of mass

---
+++ gtr.gd
@@ -0,0 +1,39 @@
+extends VehicleBody3D
+
+const MAX_STEER = 0.7
+
+var ENGINE_POWER = 300
+
+@export var boost_power = 200
+@export var boost_fuel = 10
+@export var boost_cooldown = 2
+@export var boost_deplete_rate = 1
+@export var boost_replenish_rate = 0.5
+
+var is_boosting = false
+var cooldown_timer = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	steering = move_toward(steering, Input.get_axis("move_right", "move_left") * MAX_STEER, delta * 2.5)
+	engine_force = Input.get_axis("move_down", "move_up") * ENGINE_POWER
+	
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("boost") and cooldown_timer <= 0 and boost_fuel > 0:
+		is_boosting = true
+		cooldown_timer = boost_cooldown
+		ENGINE_POWER += boost_power
+		boost_fuel -= boost_deplete_rate * delta
+		print(engine_force)
+	else:
+		is_boosting = false
+		boost_fuel = clamp(boost_fuel + boost_replenish_rate * delta, 0, 10)  
+			
+	if cooldown_timer > 0:
+		cooldown_timer -= delta
--- rigid_body_3d.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody3D
-
-
-func _on_body_entered(body):
-	if body.name == "Car":
-		apply_impulse(Vector3(0, 10, 0), Vector3.ZERO)
