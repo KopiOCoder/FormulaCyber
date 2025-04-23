9d96099c494408d379869f9db49025c3e30f89d1
KopiOCoder <wesleylimkopi@gmail.com>
2025-04-23 15:23:09 +0800

    Made camera more dynamic

--- gtr.gd
+++ gtr.gd
@@ -9,19 +9,27 @@ var ENGINE_POWER = 300
 @export var boost_cooldown = 2
 @export var boost_deplete_rate = 1
 @export var boost_replenish_rate = 0.5
+@onready var camera_pivot = $CameraPivot
+@onready var camera_3d = $CameraPivot/Camera3D
 
 var is_boosting = false
 var cooldown_timer = 0
+var look_at
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	look_at = global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	steering = move_toward(steering, Input.get_axis("move_right", "move_left") * MAX_STEER, delta * 2.5)
 	engine_force = Input.get_axis("move_down", "move_up") * ENGINE_POWER
+	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20)
+	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5)
+	look_at = look_at.lerp(global_position + linear_velocity, delta * 5)
+	camera_3d.look_at(look_at)
 	
 func _physics_process(delta):
 	
