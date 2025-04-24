bf3d9f05985e2e271ebc73b92b4000b48ca605ca
Yuuki-lost <preston1329@gmail.com>
2025-04-24 10:44:31 +0800

    Added another ui

---
+++ Loading_Menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://testing_level.tscn")
+
+
+
+
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://settings_menu.tscn")
+
+
+func _on_tutorial_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	get_tree().quit()
+	
