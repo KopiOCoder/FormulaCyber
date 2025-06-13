9fd9cffad2c9c6ce5933c7e35770be37c1bca5be
Yuuki-lost <preston1329@gmail.com>
2025-06-14 00:27:48 +0800

    gacha tweaking

--- cars/gacha_spinner.gd
+++ cars/gacha_spinner.gd
@@ -9,12 +9,35 @@ var is_spinning := false
 @onready var label: Label = $CarNameLabel
 
 func start_spin(_car_list: Array[CarData], _final_car: CarData):
+	if SaveData.money < 1000:
+		label.text = "Not enough Money!"
+		label.modulate = Color.RED
+		await get_tree().create_timer(2.0).timeout
+		hide()
+		return
+	SaveData.money -= 1000
+	SaveData.save_game()
+	
 	car_list = _car_list
 	final_car = _final_car
 	is_spinning = true
 	show()
 
 	spin_animation()
+
+func get_color_for_rarity(rarity: String) -> Color:
+	match rarity:
+		"Common":
+			return Color.WHITE
+		"Rare":
+			return Color.CYAN
+		"Epic":
+			return Color.MEDIUM_PURPLE
+		"Legendary":
+			return Color.GOLD
+		_:
+			return Color.GRAY
+
 
 func spin_animation() -> void:
 	var spin_time = 2.5
@@ -23,7 +46,8 @@ func spin_animation() -> void:
 
 	while elapsed < spin_time:
 		var current = car_list.pick_random()
-		label.text = current.name
+		label.text = final_car.name
+		label.modulate = get_color_for_rarity(final_car.rarity)
 		await get_tree().create_timer(delay).timeout
 		elapsed += delay
 		delay *= 1.08  # Slow down
